74942d142f022245ad9e9de52fc294b1c260de2e
julien essola <95610706+essolajoseph@users.noreply.github.com>
2023-01-26 15:31:49 +0100

    download pdf

---
+++ app/Http/Controllers/GeneratePdf.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+// use Barryvdh\DomPDF\PDF;
+use Dompdf\Dompdf;
+use Illuminate\Http\Request;
+
+class GeneratePdf extends Controller
+{
+    function index(Request $resquest){
+      
+    
+        @include ('bootstraps');
+ 
+    $dompdf = new Dompdf();
+    $user = User::where([
+        "id" => "1"
+       ])->get();
+    
+    $dompdf->loadHtml(view('index',compact('user')));
+
+    // $dompdf->setBasePath(__DIR__ . '/css/releve.css"');
+
+    $dompdf->setPaper('A4');
+    
+
+    $dompdf->render();
+    
+
+    $dompdf->stream("qrcode",[
+      "Attachment" => true
+  ]);
+
+ } 
+}
