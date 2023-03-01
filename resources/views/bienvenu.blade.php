b8183613071bfd15702a6f1cf38f928a5c5adac2
julien essola <95610706+essolajoseph@users.noreply.github.com>
2023-03-01 07:45:10 +0100

    public

--- resources/views/bienvenu.blade.php
+++ resources/views/bienvenu.blade.php
@@ -6,12 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Authentification des Docuements</title>
     <link rel="stylesheet" id="theme_link" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.3.1/materia/bootstrap.min.css" />
-    <link rel="stylesheet" href="{{asset ('navbar-fixed-right.min.css')}}">
-    <link rel="stylesheet" href="{{asset ('navbar-fixed-left.min.css')}}">
-    <link rel="stylesheet" href="{{asset ('docs.css')}}">
+    <link rel="stylesheet" href="{{asset ('public/navbar-fixed-right.min.css')}}">
+    <link rel="stylesheet" href="{{asset ('public/navbar-fixed-left.min.css')}}">
+    <link rel="stylesheet" href="{{asset ('public/docs.css')}}">
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="{{asset ('docs.js')}}"></script>
+    <script src="{{asset ('public/docs.js')}}"></script>
     <script async defer src="https://buttons.github.io/buttons.js"></script>
 </head>
 
