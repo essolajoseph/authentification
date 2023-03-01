e1ffcfa0404a14d4105ebcac97e892520795c72f
julien essola <95610706+essolajoseph@users.noreply.github.com>
2023-03-01 07:00:17 +0100

    gcgcgc

--- resources/views/bienvenu.blade.php
+++ resources/views/bienvenu.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" id="theme_link" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.3.1/materia/bootstrap.min.css" />
     <link rel="stylesheet" href="navbar-fixed-right.min.css">
     <link rel="stylesheet" href="navbar-fixed-left.min.css">
-    <link rel="stylesheet" href="resources/js/docs.js">
+    <link rel="stylesheet" href="{{asset('resources/js/docs.js')}}">
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     
