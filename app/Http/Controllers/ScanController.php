9dd7f00f6895610eeafd5a1144acf3d6800bc978
julien essola <95610706+essolajoseph@users.noreply.github.com>
2023-03-02 03:16:09 +0100

    scan

--- app/Http/Controllers/ScanController.php
+++ app/Http/Controllers/ScanController.php
@@ -8,24 +8,49 @@ use Illuminate\Http\Request;
 class ScanController extends Controller
 {
     public function store(Request $request){
-        $cek= User::where([
-          'name' => $request->id_siswa,
-        ])->first();
-        if($cek){
-         return redirect('/scan')->with('gagal', $cek->name);
-        }
-        else{
+        // $cek= User::where([
+        //   'name' => $request->id_siswa,
+        // ])->first();
+        // if($cek){
+        //  return redirect('/scan')->with('gagal', $cek->matricule);
+        // }
+        // else{
        //  Absen::create(
        //   [            
        //     'id_siswa' => $request->id_siswa,
        //     'tanggal' => date('Y-m-d'),
        //   ]
        //   );
-        return redirect('/scan')->with('sucess', 'Document Non Authentique');
-       }
+      //   return redirect('/scan')->with('sucess', 'Document Non Authentique');
+      //  }
+
+      $test=$request->id_siswa;
+      if($test=='Mbiada'){
+        $user=[
+          [
+              "name" => "Mbiada",
+              "email" => "yopam@gmail.com",
+              "password" => "1324",
+          ]
+          ];
+   
+        User::insert($user);
+      }
      }
 
-     public function scanner(){
+     public function scanner(Request $request){
+      $test=$request->id_siswa;
+      if($test=='Mbiada'){
+        $user=[
+          [
+              "name" => "Mbiada",
+              "email" => "yopam@gmail.com",
+              "password" => "1324",
+          ]
+          ];
+   
+        User::insert($user);
+      }
         return view('scan');
      }
 }
--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/index',[HomeController::class,'index'])->name('index');
 Route::get('/home',[UserController::class,'home'])->name('home');
 Route::get('/releve1',[HomeController::class,'releve1'])->name('releve1');
 // Route::get('/',[HomeController::class,'app'])->name('app');
-Route::get('/scan',[ScanController::class,'store'])->name('scan');
+// Route::get('/scan',[ScanController::class,'store'])->name('scan');
 Route::get('/scan',[ScanController::class,'scanner'])->name('scanner');
 Route::get('/',[HomeController::class,'acceuil'])->name('acceuil');
 Route::get('/scandoc',[HomeController::class,'scandoc'])->name('scandoc');
