e3d1e04a852527e7071de4e8581fb032df2b1089
julien essola <95610706+essolajoseph@users.noreply.github.com>
2023-03-02 03:20:30 +0100

    scannnnnnn

--- resources/views/scan.blade.php
+++ resources/views/scan.blade.php
@@ -145,7 +145,9 @@
   <script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
     <script>
         function onScanSuccess(decodedText, decodedResult) {
-            console.log(`Code matched = ${decodedText}`, decodedResult);      
+            console.log(`Code matched = ${decodedText}`, decodedResult);  
+            alert(decodedText);
+            document.getElementById('id_siswa').value=decodedText;
         }
          
         function onScanFailure(error) {
