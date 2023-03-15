cca46f47bec31c4db6ed1cda075596d678749154
julien essola <95610706+essolajoseph@users.noreply.github.com>
2023-03-15 03:34:03 +0100

    connexion au serveur

--- app/Models/Etudiant.php
+++ app/Models/Etudiant.php
@@ -3,10 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-//use Illuminate\Database\Eloquent\Model;
-use Jenssegers\Mongodb\Eloquent\Model ;
+use Jenssegers\Mongodb\Eloquent\Model;
 
-class Releve extends Model
+
+class Etudiant extends Model
 {
     use HasFactory;
 }
--- config/database.php
+++ config/database.php
@@ -42,6 +42,11 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('DB_DSN'),
+            'database' => env('DB_DATABASE', 'homestead'),
+        ],
 
         'mysql' => [
             'driver' => 'mysql',
